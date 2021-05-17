f27c5efb0adbed912bbae2da86cb075a4b2ae87e
mdelaosa <m.delaosam@gmail.com>
2021-05-17 12:39:54 +0200

    Abre página de error

--- P1/tienda.js
+++ P1/tienda.js
@@ -8,6 +8,13 @@ const PUERTO = 8000
 //-- Comprobación
 console.log("Servidor arrancado");
 
+const mime = {
+  'html' : 'text/html',
+  'css'  : 'text/css',
+  'png'  : 'image/png',
+  'ico'  : 'image/x-icon'
+};
+
 //-- Creación del servidor. Mensaje de control en terminal
 const server = http.createServer((req, res) => {
   console.log("- - Petición recibida - -\n");
@@ -37,18 +44,32 @@ const server = http.createServer((req, res) => {
   
   //-- Lectura fichero
   fs.readFile(filename, function(err, data) {
-    let mime = "text/html"
+    let mime = 'text/html';
+    //-- Imagenes
+    if (type_file == 'png') {
+      mime = "image/png";
+    }
+    //-- CSS
+    if (type_file == "css"){
+      mime = "text/css";
+    }
+    //-- Icono
+    if (type_file == "ico"){
+      mime = "text/plain";
+    }
     //-- Fichero no encontrado. Devolver mensaje de error
     if (err) {
       res.writeHead(404, {'Content-Type': mime});
-      res.write("Not Found");
-      res.end();
+      filename = "error.html"; 
+      data = fs.readFileSync(filename);
+      //-- res.write("Not Found");
+      //-- res.end();
     }else{
       //-- Si no da error: 200 OK
       res.writeHead(200, {'Content-Type': mime});
-      res.write(data);
-      res.end();
     }
+    res.write(data);
+    res.end();
   });
 
 });
