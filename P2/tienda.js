446aaf68429164420e6fc81885a46c3a8c7523ca
mdelaosa <m.delaosam@gmail.com>
2021-05-27 23:21:59 +0200

    creación archivos + edit tienda

--- P2/tienda.js
+++ P2/tienda.js
@@ -6,6 +6,35 @@ const url = require('url');
 //-- Puerto
 const PUERTO = 9000
 
+//-- Principal y error
+const MAIN = fs.readFileSync('tienda.html');
+const ERROR = fs.readFileSync('error.html');
+
+//-- Productos y carrito
+const PRODUCTO1 = fs.readFileSync('harry.html','utf-8');
+const PRODUCTO2 = fs.readFileSync('shawn.html','utf-8');
+const PRODUCTO3 = fs.readFileSync('atl.html','utf-8');
+const CARRITO = fs.readFileSync('carrito.html','utf-8');
+
+//-- Registo json + estructura de tienda
+const FICH_JSON = "tienda.json";
+const TIENDA_JSON = fs.readFileSync(FICH_JSON);
+const TIENDA = JSON.parse(TIENDA_JSON);
+
+//-- Formularios
+const FORMULARIO = fs.readFileSync('form-pedido.html');
+const FORMULARIO_ERROR = fs.readFileSync('form-pedido-error.html');
+const LOGIN = fs.readFileSync('form-login.html');
+const LOGIN_ERROR = fs.readFileSync('form-login-error.html');
+
+const mime_type = {
+  'html' : 'text/html',
+  'css'  : 'text/css',
+  'png'  : 'image/png',
+  'ico'  : 'image/x-icon'
+};
+let mime = mime_type[type_file];
+
 //-- Comprobación
 console.log("Servidor arrancado");
 
@@ -36,16 +65,7 @@ const server = http.createServer((req, res) => {
 
   //-- Lectura fichero
   fs.readFile(filename, function(err, data) {
-
-    const mime_type = {
-      'html' : 'text/html',
-      'css'  : 'text/css',
-      'png'  : 'image/png',
-      'ico'  : 'image/x-icon'
-    };
-    
-    let mime = mime_type[type_file];
-    
+  
     //-- Fichero no encontrado. Devolver mensaje de error
     if (err) {
       res.writeHead(404, {'Content-Type': mime});
