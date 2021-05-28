182483d6a742085fb3f21832bee2ff01609b89e4
mdelaosa <m.delaosam@gmail.com>
2021-05-29 01:34:06 +0200

    Escribiendo funciona la primera vez

--- P3/public/chat-client.js
+++ P3/public/chat-client.js
@@ -6,17 +6,27 @@ const enviar = document.getElementById("enviar");
 //-- Conexión con el server
 const socket = io();
 
+let escribiendo = false;
+
 //-- Mensaje recibido
 socket.on("message", (msg) =>{
-  display.innerHTML += '<p>' + '> ' + msg + '</p>';
+    display.innerHTML += '<p>' + '> ' + msg + '</p>';
 });
+
+mensaje.oninput = () => {
+    if (!escribiendo){
+        socket.send("Otra maruja está escribiendo información...");
+        escribiendo = true; 
+    }
+}
 
 //-- Envío de mensaje al pulsar enviar
 enviar.onclick = () => {
-  if (mensaje.value){
-    socket.send(mensaje.value);
-    console.log("Mensaje enviado");
-  }
-  //-- Borrar mensaje
-  mensaje.value = "";
+    if (mensaje.value){
+        socket.send(mensaje.value);
+        console.log("Mensaje enviado");
+    }
+    //-- Borrar mensaje
+    mensaje.value = "";
 }
+
