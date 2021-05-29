758b51582e8c4be848f1375c4cbc5e08d17b01f6
mdelaosa <m.delaosam@gmail.com>
2021-05-29 02:02:00 +0200

    Mejora audio introducido

--- P3/chat-server.js
+++ P3/chat-server.js
@@ -5,11 +5,11 @@ const colors = require('colors');
 
 const PUERTO = 9000;
 
-const commandos = "Los comandos disponibles son: /help, /list, /hello y /date";
-const welcome = "Has llegado al servidor perfecto para marujear";
-const bye = "¡Adiós!";
-const hello = "¡Holi! Esperamos que tengas información que contar";
-const usuario = "Alguien nuevo quiere cotillear";
+const commandos = 'Los comandos disponibles son: /help, /list, /hello y /date';
+const welcome = '-- TODA LA INFORMACIÓN ESCRITA EN ESTE CHAT SERÁ CONFIDENCIAL -- <br>  > Has llegado al servidor perfecto para marujear';
+const bye = '¡Adiós!';
+const hello = '¡Holi! Esperamos que tengas información que contar';
+const usuario = 'Alguien nuevo quiere cotillear';
 
 //-- Server
 const app = express();
@@ -20,7 +20,7 @@ let connect_count = 0;
 
 //-- Entrada web
 app.get('/', (req, res) => {
-  res.send('Acabas de entrar en la ciudad que no duerme pero está disponible <p><a href="/index.html">aquí</a></p>');
+    res.send('Acabas de entrar en la ciudad que no duerme pero está disponible <p><a href="/index.html">aquí</a></p>');
 });
 
 app.use('/', express.static(__dirname +'/'));
@@ -31,14 +31,14 @@ app.use(express.static('public'));
 //-- Websockets
 io.on('connection', (socket) => {
   
-  //-- Nuevo usuario  
-  console.log('-- ¡ALERTA! NUEVA MARUJA --'.pink);
-  connect_count += 1;
-  socket.send(welcome);
-  socket.broadcast.emit('message', usuario);
+    //-- Nuevo usuario  
+    console.log('-- ¡ALERTA! NUEVA MARUJA --'.pink);
+    connect_count += 1;
+    socket.send(welcome);
+    socket.broadcast.emit('message', usuario);
 
-  //-- Desconexión
-  socket.on('disconnect', function(){
+    //-- Desconexión
+    socket.on('disconnect', function(){
     console.log('-- FIN CONEXIÓN --'.pink);
     socket.broadcast.emit('message', bye);
     connect_count -= 1;
@@ -46,32 +46,32 @@ io.on('connection', (socket) => {
 
   //-- Mensaje a todos los usuarios
   socket.on("message", (msg)=> {
-    console.log("Mensaje: " + msg.yellow);
+    console.log('Mensaje: ' + msg.yellow);
 
     const date = new Date(Date.now());
 
     if (msg.startsWith('/')) {
-      console.log("Comandos".blue);
+      console.log('Comandos'.blue);
       switch(msg){
         case '/help':
-          console.log("Lista de comandos".blue);
+          console.log('Lista de comandos'.blue);
           socket.send(commandos);
           break;
         case '/list':
-          console.log("Lista de usuarios".blue);
-          socket.send("Hay un total de " + connect_count + " marujas en la ciudad.");
+          console.log('Lista de usuarios'.blue);
+          socket.send('Hay un total de ' + connect_count + ' marujas en la ciudad.');
           break;
         case '/hello':
-          console.log("Holi".blue);
+          console.log('Holi'.blue);
           socket.send(hello);
           break;
         case '/date':
-          console.log("Fecha".blue);
+          console.log('Fecha'.blue);
           socket.send(date);
           break;
         default:
-          console.log("Not Found".blue);
-          socket.send("404 Not Found. Comando no reconocido, prueba de nuevo. Los comandos están en /help");
+          console.log('Not Found'.blue);
+          socket.send('404 Not Found. Comando no reconocido, prueba de nuevo. Los comandos están en /help');
           break;
       }
     } else {
@@ -82,4 +82,4 @@ io.on('connection', (socket) => {
 
 //-- Lanzar el server
 server.listen(PUERTO);
-console.log("Escuchando en puerto: " + PUERTO);
+console.log('Escuchando en puerto: ' + PUERTO);
--- P3/public/chat-client.js
+++ P3/public/chat-client.js
@@ -1,7 +1,8 @@
 //-- Cuadrados chat
-const display = document.getElementById("display");
-const mensaje = document.getElementById("mensaje");
-const enviar = document.getElementById("enviar");
+const display = document.getElementById('display');
+const mensaje = document.getElementById('mensaje');
+const enviar = document.getElementById('enviar');
+const audio = new Audio('click.mp3');
 
 //-- Conexión con el server
 const socket = io();
@@ -9,13 +10,13 @@ const socket = io();
 let escribiendo = false;
 
 //-- Mensaje recibido
-socket.on("message", (msg) =>{
-    display.innerHTML += '<p>' + '> ' + msg + '</p>';
+socket.on('message', (msg) =>{
+    display.innerHTML += '<p>' + ' > ' + msg + '</p>';
 });
 
 mensaje.oninput = () => {
     if (!escribiendo){
-        socket.send("Otra maruja está escribiendo información...");
+        socket.send('Otra maruja está escribiendo información...');
         escribiendo = true; 
     }
 }
@@ -24,7 +25,19 @@ mensaje.oninput = () => {
 enviar.onclick = () => {
     if (mensaje.value){
         socket.send(mensaje.value);
-        console.log("Mensaje enviado");
+        console.log('Mensaje enviado');
+        audio.play();
+    }
+    //-- Borrar mensaje
+    mensaje.value = "";
+}
+
+//-- Envío del mensaje al pulsar enter
+mensaje.onchange = () => {
+    if (mensaje.value){
+        socket.send(mensaje.value);
+        console.log('Mensaje enviado');
+        audio.play();
     }
     //-- Borrar mensaje
     mensaje.value = "";
